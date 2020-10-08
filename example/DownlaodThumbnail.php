1a9c7925a5f678de0aa9c2e80fe02081c3275a34
bapi <mail2bapi@astrosoft.co.in>
2020-10-08 17:49:10 +0530

    YoutubeDownloader all example scripts are updated

--- example/DownlaodThumbnail.php
+++ example/DownlaodThumbnail.php
@@ -5,8 +5,8 @@
  * Time: 11:28 PM
  **/
 
-
-use mail2bapi\YoutubeDownloader\YoutubeDownloader;
+include_once '../vendor/autoload.php';
+use Mail2bapi\YouTubeDownloader\YoutubeDownloader;
 
 $yt = new YoutubeDownloader();
 $videos = $yt->getVideoDetail('https://www.youtube.com/watch?v=Pg4XhiV_72Y');
